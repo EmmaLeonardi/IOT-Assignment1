bc0c179d6c3a726cae4d1d63f75f1e67f11a699f
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-10-06 20:03:42 +0200

    Created file to define the game constants

---
+++ Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONST_H
+#define CONST_H
+
+//The number of leds and buttons present in the game
+#define N 4
+
+//The easiest difficulty
+#define LVMIN 1
+//The hardest difficulty
+#define LVMAX 4
+
+#endif
