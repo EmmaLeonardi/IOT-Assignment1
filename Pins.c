bd7bce0dc2638eba561943fe4a924909106e3ef9
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-10-06 20:05:01 +0200

    Created files to define the pin numbers

---
+++ Pins.c
@@ -0,0 +1,4 @@
+#include<Pins.h>
+
+const int BPins[N]={7,6,5,4};
+const int LPins[N]={11,10,9,8};
---
+++ Pins.h
@@ -0,0 +1,20 @@
+#ifndef PINS_H
+#define PINS_H
+
+#include<Constants.h>
+
+//The pin of the red led
+#define LS 12
+
+//The analog pin of the potentiometer
+#define Pot 1
+
+//The min value of the potentiomenter
+#define POTMIN 0
+//The max value of the potentiomenter
+#define POTMAX 1023
+
+extern const int* BPins[N];
+extern const int* LPins[N];
+
+#endif
