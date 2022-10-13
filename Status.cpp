8990e6cbd7e064c05537431a0f62c785189d6294
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-10-13 11:59:28 +0200

    Created the library to sinc the pattern with the led status

---
+++ Status.cpp
@@ -0,0 +1,30 @@
+#include "Status.h"
+
+/*Turns off all the leds in the led array, and sets the status accordingly*/
+void turnAllOff(int *ledArray, bool *status, int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        digitalWrite(ledArray[i], LOW);
+        status[i] = false;
+    }
+}
+
+/*Turns on all the leds in the led array, and sets the status accordingly*/
+void turnAllOn(int *ledArray, bool *status, int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        digitalWrite(ledArray[i], HIGH);
+        status[i] = true;
+    }
+}
+
+/*Copies the pattern in the array to the status array*/
+void setStatusAsGiven(bool *pattern, bool *status, int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        status[i] = pattern[i];
+    }
+}
---
+++ Status.h
@@ -0,0 +1,12 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+/*Turns off all the leds in the led array, and sets the status accordingly*/
+void turnAllOff(int *ledArray, bool *status, int SIZE);
+
+/*Turns on all the leds in the led array, and sets the status accordingly*/
+void turnAllOn(int *ledArray, bool *status, int SIZE);
+
+/*Copies the pattern in the array to the status array*/
+void setStatusAsGiven(bool *pattern, bool *status, int SIZE);
+#endif
