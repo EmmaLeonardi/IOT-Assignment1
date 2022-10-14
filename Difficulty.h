0971d04c42f3174866a7c0d8b46adc195f8ef275
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-10-14 09:33:12 +0200

    Added constants for the time to wait after a penality and after a game over

--- Difficulty.h
+++ Difficulty.h
@@ -1,14 +1,20 @@
 #ifndef DIFFICULTY_H
 #define DIFFICULTY_H
 
-//The minimum time to wait for the pattern to appear
+// The minimum time to wait for the pattern to appear
 #define RANDOMWAITMIN 2
 
-//The maximum time to wait for the pattern to appear
+// The maximum time to wait for the pattern to appear
 #define RANDOMWAITMAX 5
 
-//The maximum number of penalty before game over
+// The maximum number of penalty before game over
 #define PENALTYMAX 3
+
+// The time to wait after a game over before a new game can be started
+#define GAMEOVERWAIT 10
+
+// The time to turn on the penality led after a penality
+#define PENALITYLEDON 1
 
 /*Returns the start time for the difficulty level selected.
 The level has to be between 1 (easiest) and LV (hardest, defined in constants), otherwise returns -1*/
