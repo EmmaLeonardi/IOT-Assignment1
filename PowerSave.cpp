1e8a22b249923fa9a3dc03bfa5f2cd9bdbf50863
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-10-18 12:43:12 +0200

    Formatting changes, added debounce code to wakeup call

--- PowerSave.cpp
+++ PowerSave.cpp
@@ -3,24 +3,35 @@
 #include <EnableInterrupt.h>
 #include "PowerSave.h"
 #include "Pins.h"
+#define DEBOUNCED 200
+
 
 /*Calling this function enables sleep mode,
 attaches interrupts to all the button pins to wakeup the Arduino*/
-void sleep(){
+void sleep()
+{
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
-    for(int i=0; i<N; i++){
-        enableInterrupt(BPins[i],wakeup,CHANGE);
+    for (int i = 0; i < N; i++)
+    {
+        enableInterrupt(BPins[i], wakeup, RISING);
     }
     sleep_mode();
-
 }
 
 /*Calling this function disables sleep mode,
 unconnects all interrupts to all the button pins*/
-void wakeup(){
-    sleep_disable();
-    for(int i=0; i<N; i++){
-        disableInterrupt(BPins[i]);
+void wakeup()
+{
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+    if (interrupt_time - last_interrupt_time > DEBOUNCED)
+    {
+        last_interrupt_time = interrupt_time;
+        sleep_disable();
+        for (int i = 0; i < N; i++)
+        {
+            disableInterrupt(BPins[i]);
+        }
     }
 }
